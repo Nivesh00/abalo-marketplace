d184894f9ccf4268e628f4525f384c9fa1cd7951
Nivesh1 <ramlochunnivesh@gmail.com>
2023-04-19 01:31:28 +0200

    Aufgabe 10 gemacht

---
+++ playground-nivesh/abalo/public/js/M02/warenkorb.js
@@ -0,0 +1,136 @@
+"use strict";
+
+function Warenkorb()
+{
+    let korb = document.createElement('div');
+    korb.setAttribute('id', 'korb');
+
+    let basket = document.getElementById('warenkorb');
+
+    korb.style.display = 'none';
+    korb.style.height = '200px';
+    korb.style.width = '20%';
+    korb.style.marginRight = '0';
+    korb.style.textAlign = 'center';
+    korb.style.border = '2px solid black';
+
+    korb.innerHTML =
+
+        '<div id="items" style="text-align: center; height: 50px; font-weight: bolder; background-color:' +
+        ' lightblue;"></br>In meinem Warenkorb</div>';
+
+    basket.addEventListener('mouseover', function ()
+    {
+        korb.style.display = 'block';
+        korb.style.position = 'absolute';
+        korb.style.top = '30px';
+        korb.style.right = '10px';
+        korb.style.width = '400px';
+        korb.style.height = '600px';
+        korb.style.backgroundColor = 'lightgrey';
+        document.body.style.cursor = 'pointer';
+    });
+
+    basket.addEventListener('mouseout', function ()
+    {
+        korb.style.display = 'none';
+    });
+
+    korb.addEventListener('mouseover', function ()
+    {
+        korb.style.display = 'block';
+        korb.style.position = 'absolute';
+        korb.style.top = '30px';
+        korb.style.right = '10px';
+        korb.style.width = '400px';
+        korb.style.height = '600px';
+        korb.style.overflowY = 'auto';
+        korb.style.overflowX = 'hidden';
+        korb.style.backgroundColor = 'lightgrey';
+        document.body.style.cursor = 'pointer';
+    });
+
+    korb.addEventListener('mouseout', function ()
+    {
+        korb.style.display = 'none';
+    });
+
+
+    let item_list = [];
+    let table = document.getElementById('my_table');
+
+    for(let row of table.rows)
+    {
+        let item_name = null;
+        for(let cell of row.cells)
+        {
+            if(cell.id === 'name')
+                item_name = cell.innerText;
+            if(cell.id === 'add')
+            {
+                let add_btn = document.createElement('button');
+                add_btn.innerText = '+';
+                add_btn.style.marginLeft = '10px';
+                add_btn.setAttribute('value', item_name);
+                add_btn.addEventListener('click', function ()
+                {
+                    if(add_btn.innerText === '+')
+                    {
+                        add_btn.innerText = '-';
+                        item_list.push(add_btn.value);
+
+                        let in_korb = document.createElement('div');
+                        in_korb.innerText = add_btn.value;
+                        in_korb.setAttribute('id', add_btn.value);
+                        in_korb.setAttribute('class', 'my_items');
+                        in_korb.style.display = 'block';
+                        in_korb.style.padding = '5px';
+                        in_korb.style.textAlign = 'center';
+                        korb.append(in_korb);
+
+                        let my_items = document.getElementsByClassName('my_items');
+                        for(let i of my_items)
+                        {
+                            i.addEventListener('mouseover', function()
+                            {
+                                i.style.backgroundColor = 'white';
+                            });
+                            i.addEventListener('mouseout', function()
+                            {
+                                i.style.backgroundColor = 'lightgray';
+                            });
+
+                        }
+                    }
+                    else
+                    {
+                        add_btn.innerText = '+';
+                        let index = item_list.indexOf(add_btn.value);
+                        if (index > -1)
+                        { // only splice array when item is found
+                            item_list.splice(index, 1);
+                            // 2nd parameter means remove one item only
+                        }
+
+                        let in_korb = document.getElementById(add_btn.value);
+                        in_korb.remove();
+
+                    }
+                });
+                cell.append(add_btn);
+            }
+        }
+    }
+
+
+    let names = document.querySelectorAll('[id="name"]');
+    let adds = document.querySelectorAll('[id="btn"]');
+
+
+
+
+
+    basket.append(korb);
+}
+
+window.onload = () => Warenkorb();
--- playground-nivesh/abalo/resources/views/article.blade.php
+++ playground-nivesh/abalo/resources/views/article.blade.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="{{ asset('js/M02/warenkorb.js') }}"></script>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/M02/warenkorb.css') }}">
     <title>Aufgabe 10</title>
 </head>
 <body>
-<main>
+
+<header>
+    <div id="big_container">
+        <div></div>
+        <div></div>
+        <div id="warenkorb">Mein Warenkorb</div>
+    </div>
+</header>
+    <div id="big_page">
+    <div></div>
+    <div>
+    <main>
     <h1>Artikelübersicht</h1>
 
     @if(empty($myResult))
@@ -17,19 +30,20 @@
         Kein Artikel gefunden!
         Bitte versuchen Sie erneut.
     @else
-        <table>
+        <table id="my_table">
             <thead>
             <tr>
                 <th></th>
                 <th>Name</th>
                 <th>Preis</th>
                 <th>Beschreibung</th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
             @foreach($myResult as $result)
                 <tr>
-                    <td>
+                    <td id="img_cell">
                         @if(file_exists(public_path('/storage/image_folder/M1_pictures/' .
                         $result->id .'
                         .png')))
@@ -41,19 +55,19 @@
                         @elseif(file_exists(public_path('/storage/image_folder/M1_pictures/'
                         . $result->id .'.jpg')))
 
-                            <img src="{{ '../storage/image_folder/M1_pictures/' .
+                            <img class="product" src="{{ '../storage/image_folder/M1_pictures/' .
                             $result->id .'.jpg' }}"
                                  alt="Bild konnte nicht geladen werden für jpg">
 
                         @else
-                            <img src="{{ storage_path
-                            ('../storage/Images_misc/no_image_found.png') }}"
+                            <img class="product" src="{{ '../storage/image_folder/Images_misc/no_image_found.png' }}"
                                  alt="Kein Bild verfügbar">
                         @endif
                     </td>
-                    <td>{{$result->ab_name}}</td>
-                    <td>{{$result->ab_price}}</td>
-                    <td>{{$result->ab_description}}</td>
+                    <td id="name">{{$result->ab_name}}</td>
+                    <td id="price">{{$result->ab_price}}€</td>
+                    <td id="description">{{$result->ab_description}}</td>
+                    <td id="add"></td>
                 </tr>
             @endforeach
             </tbody>
@@ -62,5 +76,8 @@
     @endif
 
 </main>
+    </div>
+    <div></div>
+    </div>
 </body>
 </html>
