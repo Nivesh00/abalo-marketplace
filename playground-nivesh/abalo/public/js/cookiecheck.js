d49c999560a76150ec965a6a8daa674248fdde67
Nivesh1 <ramlochunnivesh@gmail.com>
2023-04-23 01:58:14 +0200

    Cookiecheck.js updated: button looks better and timer for cookie info programmed

--- playground-nivesh/abalo/public/js/cookiecheck.js
+++ playground-nivesh/abalo/public/js/cookiecheck.js
@@ -150,7 +150,22 @@ class CookieClass{
     static interest = false;
     static advertisement = false;
 
-    static description = 'my full description';
+    static description =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet semper nisi.' +
+        ' Morbi ultricies non metus eu tempus. Sed ut fermentum eros. Donec vulputate auctor nisl, ' +
+        'eget efficitur risus placerat non. Curabitur pretium varius accumsan. Curabitur eget nunc in ' +
+        'metus efficitur feugiat. Mauris gravida leo mauris, et consectetur odio aliquet eget. In congue ' +
+        'ex ut nulla sodales, et sollicitudin tellus fringilla. Duis euismod volutpat libero. Aliquam venenatis ' +
+        'aliquet lacus venenatis suscipit. Fusce congue ac risus eget cursus. Quisque nec commodo ligula, efficitur ' +
+        'sollicitudin sem. Morbi nec ullamcorper leo.' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet semper nisi.' +
+        ' Morbi ultricies non metus eu tempus. Sed ut fermentum eros. Donec vulputate auctor nisl, ' +
+        'eget efficitur risus placerat non. Curabitur pretium varius accumsan. Curabitur eget nunc in ' +
+        'metus efficitur feugiat. Mauris gravida leo mauris, et consectetur odio aliquet eget. In congue ' +
+        'ex ut nulla sodales, et sollicitudin tellus fringilla. Duis euismod volutpat libero. Aliquam venenatis ' +
+        'aliquet lacus venenatis suscipit. Fusce congue ac risus eget cursus. Quisque nec commodo ligula, efficitur ' +
+        'sollicitudin sem. Morbi nec ullamcorper leo.'
+       ;
 
     static CookieBox()
     {
@@ -158,6 +173,7 @@ class CookieClass{
 
         let empty_div = document.createElement('div');
         empty_div.style.height = '170px';
+
 
         let c_box = document.createElement('div');
         c_box.setAttribute('id', 'c_box');
@@ -173,6 +189,7 @@ class CookieClass{
         c_box.style.marginRight = 'auto';
         c_box.style.padding = '20px';
         c_box.style.borderRadius = '20px';
+        c_box.style.border = '2px solid red';
 
 
         document.body.append(c_box);
@@ -187,10 +204,20 @@ class CookieClass{
         let c_description = document.createElement('div');
         c_description.setAttribute('id', 'c_description');
 
-        c_description.innerText = this.description;
+        let inner_text = document.createElement('div');
+        inner_text.setAttribute('id', 'inner_text');
+        inner_text.style.margin = '30px 20px 0 20px';
+        inner_text.style.padding = '20px';
+        inner_text.style.height = '300px';
+        inner_text.style.overflowY = 'auto';
+        inner_text.style.background = 'white';
+        inner_text.style.border = '2px solid black'
+        inner_text.style.borderRadius = '20px';
+        inner_text.innerText = this.description;
 
         //c_description.style.w
 
+        c_description.append(inner_text);
         c_box.append(c_description);
 
     }
@@ -209,7 +236,9 @@ class CookieClass{
         close_div.setAttribute('id', 'close_div');
 
         close_div.innerHTML =
-            '<button style="border: none; float: right; padding: 10px; background-color:' +
+            '<button style="color: red;border: none; float: right; padding:' +
+            ' 10px;' +
+            ' background-color:' +
             ' lightcyan;"' +
             ' type="button"' +
             ' id="close">&#10006;</button>'
@@ -230,6 +259,8 @@ class CookieClass{
             })
             btn.addEventListener('click', function () {
                 c_box.style.display = 'none';
+                document.cookie = 'essential=true';
+
             })
         }
         //cross.style.border = '1px solid black';
@@ -239,7 +270,7 @@ class CookieClass{
 
         let empty_cookie = document.createElement('div');
         empty_cookie.setAttribute('id', 'empty_cookie');
-        empty_cookie.style.height = '70px';
+        empty_cookie.style.height = '50px';
 
         c_choice.append(empty_cookie);
 
@@ -247,20 +278,22 @@ class CookieClass{
         auswahl.setAttribute('id', 'auswahl');
 
         auswahl.innerHTML =
-            '<table style="width: 250px"><thead><tr><th><th></th></th></tr></thead>'
+            '<table style="width: 250px">' +
+            '<caption style="text-align: center; font-weight: bold">Cookie Auswahl</caption>' +
+            '<thead><tr><th><th></th></th></tr></thead>'
             + '<tbody>'
             + '<tr>'
-            + '<td>Essentielle cookies </br>(immer an)</td>'
+            + '<td>Essentielle Cookies </br>(immer an)</td>'
             + '<td class="cb"><input type="checkbox" value="essential=true" checked disabled></td>'
             + '</tr>'
 
             + '<tr>'
-            + '<td>Bewerbung cookies</td>'
+            + '<td>Werbung Cookies</td>'
             + '<td class="cb"><input id="adv" type="checkbox" value="advertisement=true"></td>'
             + '</tr>'
 
             + '<tr>'
-            + '<td>Interesse cookies</td>'
+            + '<td>Interesse Cookies</td>'
             + '<td class="cb"><input id="int" type="checkbox" value="interest=true"></td>'
             + '</tr>'
 
@@ -284,9 +317,10 @@ class CookieClass{
         speichern.setAttribute('id', 'speicher');
         speichern.style.height = '30px';
         speichern.innerHTML =
-            '<button id="speichern" style="padding: 10px;border-radius: 10px;margin-top:' +
+            '<button id="speichern" style="margin-left: 30px;padding: 10px;border-radius:' +
+            ' 10px;margin-top:' +
             ' 50px;width:' +
-            ' 100%;border: 2px' +
+            ' 210px;border: 2px' +
             ' solid' +
             ' saddlebrown;' +
             ' background-color:' +
@@ -296,20 +330,23 @@ class CookieClass{
             ' speichern</button>'
 
 
-
+        //let speichern_btn = document.getElementById('speichern');
         let speichern_btn = speichern.getElementsByTagName('button')[0];
 
         speichern_btn.addEventListener('mouseover', function ()
         {
             document.body.style.cursor = 'pointer';
+            speichern_btn.style.backgroundColor = 'lightgreen';
         })
         speichern_btn.addEventListener('mouseout', function ()
         {
             document.body.style.cursor = 'initial';
+            speichern_btn.style.backgroundColor = 'lightsalmon';
         })
         speichern_btn.addEventListener('click', function()
         {
             let selection = document.querySelectorAll('input[type=checkbox]:checked');
+
             document.cookie = 'essential=true';
             for(let ckie of selection) {
                 if (ckie.value === 'advertisement=true') {
@@ -323,7 +360,28 @@ class CookieClass{
                 }
             }
 
-            c_box.style.display = 'none';
+
+            speichern_btn.innerHTML = speichern_btn.innerHTML.replace('Auswahl speichern',
+                'Auswahl gespeichert &#10004;');
+            speichern_btn.style.backgroundColor = 'lightgrey';
+            speichern_btn.addEventListener('mouseover', function ()
+            {
+                document.body.style.cursor = 'pointer';
+                speichern_btn.style.backgroundColor = 'lightgray';
+            })
+            speichern_btn.addEventListener('mouseout', function ()
+            {
+                document.body.style.cursor = 'initial';
+                speichern_btn.style.backgroundColor = 'lightgray';
+            })
+
+
+            setTimeout(function ()
+            {
+                c_box.style.display = 'none';
+
+            },2000);
+
         })
 
         c_choice.append(auswahl);
@@ -333,7 +391,6 @@ class CookieClass{
         c_box.append(c_choice);
     }
 
-
     static CreateCookiePopUp()
     {
         this.CookieBox();
@@ -342,7 +399,8 @@ class CookieClass{
     }
 }
 
-window.onload = function ()
+window.onload = () => setTimeout(function ()
 {
-    CookieClass.CreateCookiePopUp();
-}
+    if(document.cookie.indexOf('essential=true') === -1)
+        CookieClass.CreateCookiePopUp();
+}, 2000);
